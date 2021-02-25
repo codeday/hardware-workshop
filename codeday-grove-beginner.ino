ee5b4facbb66f267a4af641726819e3b6e0111c0
Maculos <63527367+Maculos@users.noreply.github.com>
2021-02-25 01:40:52 -0600

    Screen now showing a hello world and cleaned up code.
    Cleaned up code, changed the "owo" var to the sensible name of color, display is working but is upside down (trying to fix, library is confusing) also either the library is very inefficient or my code is causing a massive memory kerfuffle as with just a hello world 77% of the memory is used. This memory issue could be left in without going over the limit by Friday but in later revisions it should be resolved.

--- codeday-grove-beginner.ino
+++ codeday-grove-beginner.ino
@@ -5,33 +5,24 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
 #include "DS1307.h"
 
 //Enables features.
-#define mech_key = true;
-//#define rtc = true;
+#define mech_key
 char kit = "beginner"; //unused rn
 
 //Board pin numbers and other variables
 char led = 4;
 char buzzer = 5;
-int owo = 0;
+int color = 0;
 int fadeSpeed = 5;
 int fadeInterval = 1;
 
-//Some premade colors UwU (in RGB)
-int purple[3] = {255,0,255};
-int yellow[3] = {255,255,0};
-int teal[3] = {0,255,255};
-int red[3] = {255,0,0};
-int green[3] = {0,255,0};
-int blue[3] = {0,0,255};
-U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R2, /* reset=*/U8X8_PIN_NONE);
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE); 
 
 //A tiny bit of logic to enable certain modules
-#ifdef rtc
-DS1307 clock;
-#endif
 #ifdef mech_key
 char button = 2;
 char pixel_pin = 3;
@@ -42,13 +33,7 @@ char button = 6;
 #endif
 
 void setup() {
-  #ifdef rtc
-  Serial.begin(9600);
-  clock.begin();
-  clock.fillByYMD(1970,1,1);
-  clock.fillByHMS(0,0,0);
-  clock.fillDayOfWeek(THU);
-  #endif
+  u8g2.begin();
   #ifdef mech_key
   pinMode(button, INPUT);
   pixel.begin();
@@ -56,20 +41,28 @@ void setup() {
   pixel.setPixelColor(0,pixel.Color(255,0,255));
   pixel.show();
   #endif
+  u8g2.clearBuffer(); 
+  u8g2.setFont(u8g2_font_ncenB08_tr);
 }
 
 void loop() {
   //fade in and out mech key led
   #ifdef mech_key
   if(digitalRead(button) == true) {
-    pixel.setPixelColor(0,pixel.Color(purple[0],purple[1],purple[2]));
+    pixel.setPixelColor(0,pixel.Color(255,0,255));
     pixel.show();
+    u8g2.clearBuffer();
+    u8g2.drawStr(10,50,"PORPL");
+    u8g2.sendBuffer();
   } else {
     while(digitalRead(button) == false) {
-      owo = owo+fadeInterval;
-      pixel.setPixelColor(0,pixel.Color(owo,owo,owo));
+      color = color+fadeInterval;
+      pixel.setPixelColor(0,pixel.Color(color,color,color));
       pixel.show();
-      if (owo <=0 || owo>=255) {
+      u8g2.clearBuffer();
+      u8g2.drawStr(10,50,"Hello World color");
+      u8g2.sendBuffer();
+      if (color <=0 || color>=255) {
         fadeInterval = -fadeInterval;
       }
       delay(fadeSpeed);
