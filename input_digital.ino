b00953101ba234015595b095738a76df7ba8b063
Lola <--replace-all>
2021-02-25 17:24:45 -0600

    add input_digital.ino

---
+++ input_digital.ino
@@ -0,0 +1,18 @@
+char led = 4;
+char button = 6;
+  
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if (digitalRead(button) == HIGH) {
+      digitalWrite(led, HIGH);
+    }
+    else {
+      digitalWrite(led, LOW);
+    }
+}
