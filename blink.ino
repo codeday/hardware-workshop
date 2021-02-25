f8078ae8afb493ccdc9c5d267fbcccef51b147bb
Lola <--replace-all>
2021-02-25 17:03:32 -0600

    add LED blink and LCD example code

---
+++ blink.ino
@@ -0,0 +1,16 @@
+//#--------------------------------------------------------------------------------------#
+//| Thanks to Digikey for providing the kits and helping us make this an amazing Codeday!|
+//#--------------------------------------------------------------------------------------#
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT); // LED pin
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);             // wait for a second
+  digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);             // wait for a second (1000 milliseconds)
+}
---
+++ helloworld_lcd.ino
@@ -0,0 +1,24 @@
+//#--------------------------------------------------------------------------------------#
+//| Thanks to Digikey for providing the kits and helping us make this an amazing Codeday!|
+//#--------------------------------------------------------------------------------------#
+
+#include <U8g2lib.h>
+
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, SCL, SDA, U8X8_PIN_NONE);  // High speed I2C
+
+void setup(void) {
+
+  u8g2.begin();
+
+}
+
+void loop(void) {
+
+  u8g2.clearBuffer();                   // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+  u8g2.drawStr(0,10,"Hello World!");    // write something to the internal memory
+  u8g2.sendBuffer();                    // transfer internal memory to the display
+  delay(1000);  
+
+}
